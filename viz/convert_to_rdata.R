660720dcc36981334f9dd3af7f5269de9b5bd8d7
theericstone <eric@flipsidecrypto.com>
2021-10-27 22:39:05 +0000

    getting this to deploy

--- viz/convert_to_rdata.R
+++ viz/convert_to_rdata.R
@@ -2,14 +2,15 @@ library(data.table)
 require(dplyr)
 library(plotly)
 
-setwd("~/git/nft-deal-score/viz")
+#setwd("~/git/nft-deal-score/viz")
 
-pred_price <- read.csv('../data/pred_price.csv') %>% as.data.table()
+pred_price <- read.csv('~/nft-deal-score/data/pred_price.csv') %>% as.data.table()
 pred_price[, token_id := as.numeric(token_id) ]
 pred_price <- pred_price[order(token_id)]
 
-attributes <- read.csv('../data/attributes.csv') %>% as.data.table()
-feature_values <- read.csv('../data/feature_values.csv') %>% as.data.table()
-sales <- read.csv('../data/sales.csv') %>% as.data.table()
+attributes <- read.csv('~/nft-deal-score/data/attributes.csv') %>% as.data.table()
+feature_values <- read.csv('~/nft-deal-score/data/feature_values.csv') %>% as.data.table()
+sales <- read.csv('~/nft-deal-score/data/sales.csv',colClasses = c("character")) %>% as.data.table()
 
-save(pred_price, attributes, feature_values, sales, file='data.Rdata')
+save(pred_price, attributes, feature_values,sales,
+     file='~/nft-deal-score/viz/data.Rdata')
